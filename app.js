392536de72249e347acc8e250b388037448bbd6b
Aurnab Das <59371873+Aurnab990@users.noreply.github.com>
2022-06-01 20:45:14 +0600

    firt button worked successfully

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-const handleClick = () =>{
+
     const value =document.getElementById('demo').addEventListener = Math.floor(Math.random()*3);
     
-    if(value==0){
-        console.log("draw")
+    const handleClick = () =>{
+        if(value == 0){
+            document.getElementById('demo').innerHTML="DRAW";
+        }
+        else if(value==1){
+            document.getElementById('demo').innerHTML="Machine Win";
+        }
+        else if(value==2){
+            document.getElementById('demo').innerHTML="You Win";
+        }
     }
-}
+    const handleClickTwo = () =>{
+        if(value == 1){
+            console.log("Draw",value);
+        }
+        else if(value==2){
+            console.log("Machine Winner",value);
+        }
+        else if(value==0){
+            console.log("You WIn",value);
+        }
+    }
+    const handleClickThree = () =>{
+        if(value == 2){
+            console.log("Draw",value);
+        }
+        else if(value==0){
+            console.log("Machine Winner",value);
+        }
+        else if(value==1){
+            console.log("You WIn",value);
+        }
+    }
+
+
+
+
