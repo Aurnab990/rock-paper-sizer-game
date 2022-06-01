f7c16b4ae4e6fbbe0669da9ffd7ff3ac74bff869
Aurnab Das <59371873+Aurnab990@users.noreply.github.com>
2022-06-01 19:44:50 +0600

    connect with js file

---
+++ app.js
@@ -0,0 +1,7 @@
+const handleClick = () =>{
+    const value =document.getElementById('demo').addEventListener = Math.floor(Math.random()*3);
+    
+    if(value==0){
+        console.log("draw")
+    }
+}
