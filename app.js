a63c05e98082dde1b433e78580ed01fdeb197c07
Aurnab Das <59371873+Aurnab990@users.noreply.github.com>
2022-06-01 21:59:24 +0600

    All are set

--- app.js
+++ app.js
@@ -4,35 +4,50 @@
     const handleClick = () =>{
         if(value == 0){
             document.getElementById('demo').innerHTML="DRAW";
+            document.getElementById('machineDemo').innerHTML="ROCK"
         }
         else if(value==1){
             document.getElementById('demo').innerHTML="Machine Win";
+            document.getElementById('machineDemo').innerHTML="PAPER"
         }
         else if(value==2){
             document.getElementById('demo').innerHTML="You Win";
+            document.getElementById('machineDemo').innerHTML="SIZER"
         }
     }
     const handleClickTwo = () =>{
         if(value == 1){
-            console.log("Draw",value);
+            document.getElementById('demo').innerHTML="DRAW";
+            document.getElementById('machineDemo').innerHTML="PAPER"
         }
         else if(value==2){
-            console.log("Machine Winner",value);
+            document.getElementById('demo').innerHTML="Machine Win";
+            document.getElementById('machineDemo').innerHTML="SIZER"
         }
         else if(value==0){
-            console.log("You WIn",value);
+            document.getElementById('demo').innerHTML="You Win";
+            document.getElementById('machineDemo').innerHTML="ROCK"
         }
     }
     const handleClickThree = () =>{
         if(value == 2){
-            console.log("Draw",value);
+            document.getElementById('demo').innerHTML="DRAW";
+            document.getElementById('machineDemo').innerHTML="SIZER"
         }
         else if(value==0){
-            console.log("Machine Winner",value);
+            document.getElementById('demo').innerHTML="Machine Win";
+            document.getElementById('machineDemo').innerHTML="ROCK"
         }
         else if(value==1){
-            console.log("You WIn",value);
+            document.getElementById('demo').innerHTML="You Win";
+            document.getElementById('machineDemo').innerHTML="PAPER"
         }
+    }
+    const handleReset = () =>{
+        location.reload();
+        return false;
+            
+
     }
 
 
